ac03152acf77cd16c46e4812b3272a377ec8ae8f
Scott Zuke <smzuke@gmail.com>
2015-08-21 18:23:59 -0400

    New tutorials

--- tutorials.php
+++ tutorials.php
@@ -3,24 +3,40 @@
 <div class="page container">
 
   <div class="row">
-    <div class="col-md-8 col-md-offset-2">
-
+    <div class="col-sm-12">
       <h1 class="page-header">Tutorials</h1>
-
-        <ul>
-          <li><a href="tutorial.php?n=htmlcss">HTML/CSS Training</a><br>
-          Materials to get started on our HTML tutorial, plus some resources for your future reference.</li>
-			    <li><a href="tutorial.php?n=media-tracking">Media Tracking</a><br>
-          A guide for Communications interns to get started tracking MEI scholars' media hits and briefings.</li>
-          <li><a href="tutorial.php?n=newsandviews">News & Views Weekly Newsletter</a><br>
-          A quick rundown for Communications interns of the steps involved in filling out MEI's weekly e-mail newsletter.</li>
-          <li><a href="tutorial.php?n=slideshow">Creating a Tutorial Slideshow</a><br>
-          These tutorials are written in simple HTML. Here's how to make one, for any staff who are interested.</li>
-          <li>More coming soon...</li>
-        </ul>
-
     </div>
   </div>
-</div>
+
+  <div id="quicklinks" class="row">
+    <div class="col-sm-4 centered">
+      <h2><a href="tutorial.php?n=htmlcss" class="quicklink"><i class="fa fa-code"></i><br>Web Code</a></h2>
+      <p>Learn HTML and CSS</p>
+    </div>
+    <div class="col-sm-4 centered">
+      <h2><a href="tutorial.php?n=media-tracking" class="quicklink"><i class="fa fa-newspaper-o"></i><br>Media Hits</a></h2>
+      <p>Track MEI Scholars in News</p>
+    </div>
+    <div class="col-sm-4 centered">
+      <h2><a href="tutorial.php?n=newsandviews" class="quicklink"><i class="fa fa-envelope-o"></i><br>N&V</a></h2>
+      <p>MEI's weekly newsletter</p>
+    </div> 
+  </div> <!-- /row -->
+
+  <div id="quicklinks" class="row">
+    <div class="col-sm-4 centered">
+      <h2><a href="tutorial.php?n=slideshow" class="quicklink"><i class="fa fa-laptop"></i><br>Slides</a></h2>
+      <p>Make your own tutorial</p>
+    </div>
+    <div class="col-sm-4 centered">
+      <h2><a href="tutorial.php?n=photo-editing" class="quicklink"><i class="fa fa-camera-retro"></i><br>Photos</a></h2>
+      <p>Edit image files for the web</p>
+    </div>
+    <div class="col-sm-4 centered">
+    
+    </div>  
+  </div> <!-- /row -->
+
+</div> <!-- /page container -->
 
 <?php include('footer.php'); ?>
