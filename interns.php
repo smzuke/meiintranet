b9e7a151ecaaa3607a8b67648f192439b9f66afd
Scott Zuke <smzuke@gmail.com>
2015-08-22 14:10:22 -0400

    Styling IDS video sidebar

--- interns.php
+++ interns.php
@@ -1,34 +1,27 @@
 <?php include('header.php'); ?>
-
 <div class="page container">
-
-
-<div class="row">
-    
-    
-    <div class="col-sm-8"> <!-- Shortcuts Column -->
-
-      <h1 class="page-header">Shortcuts</h1>
-      <ul>
-        <li><a href="https://accounts.newscred.com/login">Newscred</a><br>
-        Update MEI's newsfeeds and find licensed photos.</li>
-        <li><a href="https://ui.constantcontact.com/rnavmap/em/home/tab">Constant Contact</a><br>
-        Create event flyers and fill in News & Views, our weekly newsletter.</li>
-        <li><a href="http://www.mei.edu/user">Log in to Drupal</a><br>
-        Access the backend of www.mei.edu to create and edit event pages, update the photostream, or enter scholar quotes.</li>
-        <li><a href="https://docs.google.com/a/mei.edu/spreadsheets/d/1DPWFWz6eOGuGNFoGRKmJpURN5KhpReuWb0BmvxU7Xo0/edit?usp=sharing">
-        Media Tracking Spreadsheet</a><br>
-        Spreadsheet to record scholar activities and generate the Scholars section of the News & Views weekly newsletter.</li>
-        <li><a href="http://myemail.constantcontact.com/A-Look-at-Egypt-s-Short-and-Long-Term-Challenges---News---Views-.html?soid=1102919617474&aid=fKvkN5LLs7E">
-        Sample News & Views</a></li>
-        <li><a href="http://myemail.constantcontact.com/-Event--The-Middle-East-in-2025--Long-Term-Scenarios-and-Strategies-for-Stability.html?soid=1102919617474&aid=5Sl3nvaVwkY">
-        Sample Event Invitation</a></li>
-        <li><a href="http://myemail.constantcontact.com/Analysis---Turkey-s-Pro-Kurdish-Party-Gaining-Ground.html?soid=1102919617474&aid=XDNvBWzccMk">
-        Sample Scholar Publication</a></li>
-      </ul>
-
-      <h1 class="page-header">Useful<sup>*</sup> Tools <em>(Experimental)</em></h1>
-
+  <div class="row">
+    <div class="col-sm-8"> <!-- Spacer -->
+      <div class="white-panel col-sm-12"> <!-- Shortcuts Column -->
+        <h1 class="page-header">Shortcuts</h1>
+        <ul>
+          <li><a href="https://accounts.newscred.com/login">Newscred</a><br>
+          Update MEI's newsfeeds and find licensed photos.</li>
+          <li><a href="https://ui.constantcontact.com/rnavmap/em/home/tab">Constant Contact</a><br>
+          Create event flyers and fill in News & Views, our weekly newsletter.</li>
+          <li><a href="http://www.mei.edu/user">Log in to Drupal</a><br>
+          Access the backend of www.mei.edu to create and edit event pages, update the photostream, or enter scholar quotes.</li>
+          <li><a href="https://docs.google.com/a/mei.edu/spreadsheets/d/1DPWFWz6eOGuGNFoGRKmJpURN5KhpReuWb0BmvxU7Xo0/edit?usp=sharing">
+          Media Tracking Spreadsheet</a><br>
+          Spreadsheet to record scholar activities and generate the Scholars section of the News & Views weekly newsletter.</li>
+          <li><a href="http://myemail.constantcontact.com/A-Look-at-Egypt-s-Short-and-Long-Term-Challenges---News---Views-.html?soid=1102919617474&aid=fKvkN5LLs7E">
+          Sample News & Views</a></li>
+          <li><a href="http://myemail.constantcontact.com/-Event--The-Middle-East-in-2025--Long-Term-Scenarios-and-Strategies-for-Stability.html?soid=1102919617474&aid=5Sl3nvaVwkY">
+          Sample Event Invitation</a></li>
+          <li><a href="http://myemail.constantcontact.com/Analysis---Turkey-s-Pro-Kurdish-Party-Gaining-Ground.html?soid=1102919617474&aid=XDNvBWzccMk">
+          Sample Scholar Publication</a></li>
+        </ul>
+        <h1 class="page-header">Useful<sup>*</sup> Tools <em>(Experimental)</em></h1>
         <ul>
           <li><a href="leaveanote.php">
           Leave a Note!</a><br>
@@ -46,58 +39,39 @@
           <li><a href="javascript:var%20blogPostTitle=document.querySelector('h3.entry-title').textContent;var%20blogPostURL=document.URL;var%20blogPostSummary='<a%20style=&quot;color:%20#cc6633;%20font-weight:%20bold;%20text-decoration:%20none;&quot;%20track=&quot;on&quot;%20shape=&quot;rect&quot;%20href=&quot;'+blogPostURL+'&quot;%20linktype=&quot;1&quot;%20target=&quot;_blank&quot;>'+blogPostTitle+'</a><br><br>';alert(blogPostSummary);void(0);" class="bookmarklet">Editor's Blog Link</a>
           Drag this into your bookmarks bar. When you go to a post on the MEI Editor's Blog, click the bookmarklet, and a pop-up window will appear with the HTML code that you can drop into Constant Contact.</li>
         </ul>
-
         <br>
         <p><sup>*</sup><em>Tools may or may not be at all useful.</em></p>
-
-      <br>
-    </div> <!-- End of Shortcuts Column -->
-  
-<div class="col-sm-4">
-  
-<section id="ids-videos-sidebar">
-
-
-<h1 class="page-header">IDS Videos</h1>
-
-			
-				<div class="media"> <!-- Robert Ford -->
-				  <div class="media-left">
-				    <a class="fancybox-media" href="https://www.youtube.com/watch?v=YODxESckqTg">
-				      <img class="media-object" src="http://www.mei.edu/sites/default/files/styles/medium/public/photos/Robert%20Ford%202014%203.JPG?itok=NiAsYsXY" alt="...">
-				    </a>
-				  </div>
-				  <div class="media-body">
-				    <h4 class="media-heading"><a class="fancybox-media" href="https://www.youtube.com/watch?v=YODxESckqTg">Robert S. Ford</a></h4>
-				    July 20, 2015 - A conversation with Robert Ford, fmr. US Ambassador to Syria and Algeria, and MEI Senior Fellow.
-				  </div>
-				</div> <!-- End of Robert Ford -->	
-		
-		
-				<div class="media"> <!-- Matt Duss -->
-				  <div class="media-left">
-				    <a class="fancybox-media" href="https://www.youtube.com/watch?v=haFHEjs8SIE">
-				      <img class="media-object" src="assets/img/photos/MattDuss.jpeg" alt="...">
-				    </a>
-				  </div>
-				  <div class="media-body">
-				    <h4 class="media-heading"><a class="fancybox-media" href="https://www.youtube.com/watch?v=haFHEjs8SIE">Matt Duss</a></h4>
-				    July 21, 2015 - A conversation with Matt Duss, president of the Foundation for Middle East Peace.
-				  </div>
-				</div> <!-- End of Matt Duss -->				
-		
-	
-	</section> <!-- End Tutorials -->  
-	</div> <!--/video column-->
-  
-  
+        <br>
+      </div> <!-- End of Shortcuts Column -->
+    </div> <!-- End of Spacer -->
+    <div class="col-sm-4"> <!-- Spacer -->
+      <div id="ids-videos-sidebar" class="col-sm-12">
+        <h1>IDS Videos</h1>
+        <p>A selection of past recorded IDS talks, in case you missed one or a particular speaker wasn't available this term.</p>  			
+  			<div class="media"> <!-- Robert Ford -->
+  			  <div class="media-left">
+  			    <a class="fancybox-media" href="https://www.youtube.com/watch?v=YODxESckqTg">
+  			      <img class="media-object" src="http://www.mei.edu/sites/default/files/styles/medium/public/photos/Robert%20Ford%202014%203.JPG?itok=NiAsYsXY" alt="...">
+  			    </a>
+  			  </div>
+  			  <div class="media-body">
+  			    <h4 class="media-heading"><a class="fancybox-media" href="https://www.youtube.com/watch?v=YODxESckqTg">Robert S. Ford</a></h4>
+  			    July 20, 2015 - A conversation with Robert Ford, fmr. US Ambassador to Syria and Algeria, and MEI Senior Fellow.
+  			  </div>
+  			</div> <!-- End of Robert Ford -->	
+  			<div class="media"> <!-- Matt Duss -->
+  			  <div class="media-left">
+  			    <a class="fancybox-media" href="https://www.youtube.com/watch?v=haFHEjs8SIE">
+  			      <img class="media-object" src="assets/img/photos/MattDuss.jpeg" alt="...">
+  			    </a>
+  			  </div>
+  			  <div class="media-body">
+  			    <h4 class="media-heading"><a class="fancybox-media" href="https://www.youtube.com/watch?v=haFHEjs8SIE">Matt Duss</a></h4>
+  			    July 21, 2015 - A conversation with Matt Duss, president of the Foundation for Middle East Peace.
+  			  </div>
+  			</div> <!-- End of Matt Duss -->				 		
+      </div> <!--/video column-->
+    </div> <!-- End of spacer --> 
   </div> <!-- End of Row -->
-
-
-
-
-
-  
-</div>
-
+</div> <!-- End of Container -->
 <?php include('footer.php'); ?>
