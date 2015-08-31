6b6a7e62ead2b58c61e44c8e132376cc3d016466
Scott Zuke <smzuke@gmail.com>
2015-08-31 12:03:12 -0400

    Restoring the Media Tracking tutorial

--- assets/tutorials/media-tracking.php
+++ assets/tutorials/media-tracking.php
@@ -1,43 +1,118 @@
-<div id="slides" data-tutorial-title="Tracking Media Hits" data-toc="yes">
+<div id="slides" data-tutorial-title="Tracking Scholar Media Hits" data-toc="yes">
 
           <section>
             <h3>Introduction</h3>
-            <p>News & Views(N&V) is our weekly institutional newsletter, highlighting programs, publications, and other events at MEI. Scott will add the main body content to the newsletter, usually on Friday mornings. The rest of the newsletter, namely the Sidebar and “Scholars in the News” section, is filled in by the interns and should be ready before noon on Fridays to allow time for Scott and Kate to review it before sending out around 2pm.</p>
-            <p>You can see a sample issue of N&V <a href="http://myemail.constantcontact.com/A-Look-at-Egypt-s-Short-and-Long-Term-Challenges---News---Views-.html?soid=1102919617474&aid=fKvkN5LLs7E" target="_blank">here</a>.</p>
+            <p>MEI internally tracks all of its scholars’ appearances and media hits in data form (Media Tracking) and also externally in a weekly roundup called News & Views. Media Tracking are the data from which presentations to the Board of Governors and other important annual reviews are drawn, so they need to be regularly and accurately maintained. The tracking is generally done on a shared Google Doc (our version is: <a href="https://docs.google.com/a/mei.edu/spreadsheets/d/1DPWFWz6eOGuGNFoGRKmJpURN5KhpReuWb0BmvxU7Xo0/edit?usp=sharing" target="_blank">2015 Media Tracking</a>), with individual sheets for each new week.</p>
           </section>
           <section>
-            <h3>Sidebar Sections</h3>
+            <h3>What We Track</h3>
+            <p>There are three general types of activity we track:</p>
             <ul>
-              <li><strong>New Publications:</strong> Title, Author, and Link to articles published on MEI’s website since the last issue of N&V</li>
-              <li><strong>Upcoming Events:</strong> Date, Time, Event Title, and Link to any upcoming events that have corresponding Event pages on the site.</li>
-              <li><strong>MEJ Editor’s Blog:</strong> Linked Titles for three recent blog posts by our Middle East Journal editor, Michael Dunn.  We usually try to include two of his more research-heavy pieces, and one funny one.</li>
-              <li><strong>MAP Highlights:</strong> Linked Titles to new publications on The Middle East-Asia Project page, if any new ones have been posted in the past week.</li>
-              <li><strong>Middle East Journal:</strong> Linked Photo of the cover of the most recent MEJ. We only have to change this four times a year ;-)</li>
-              <li><strong>Follow MEI and MEI Directory:</strong> shouldn’t have to touch these.</li>
+              <li>A <strong>Media Hit</strong> is any mention of or appearance by our scholars in an article or on a broadcast program. This doesn't include a mention of our scholars/publications in the footnotes of an article or a mention of MEI in the author’s biography (at the end of the article).</li>
+              <li><strong>Scholar publications</strong> include articles in a journal/newspaper, op-ed piece, etc., written by one of our scholars.</li>
+              <li><strong>Scholar appearances/briefings</strong> public and private speaking engagements (e.g, conference, lecture for a university class, or presentation), briefings with policy makers, and Congressional testimony.</li>
+            </ul>
+            <p>
+              Be sure to regularly update the Media Tracking Google Sheet whenever you have downtime, lest all of the alerts/notifications pile up over the week. We create a new sheet for each week, as this is consistent with the frequency of News & Views.
+            </p>
+            <p>
+              The next few slides will cover how to fill in the Tracking Sheet, column by column.
+            </p>
+          </section>
+          <section>
+            <h3>Frequently Asked Questions</h3>
+            <p>
+              Sometimes, the media hits/appearances will fall in a grey area. For example, a lot of our scholars are also experts in other fields and regions, and Google Alerts may mention an article they wrote that might not necessarily be related to MEI’s research areas or regions. In that case, we generally only include the media hit if the scholar personally contacts us about it.
+            </p>
+            <p>
+              Also, watch out for repeats on the media tracker; for example, pieces that go up on NPR will also be republished by its many affiliates, which means you may have the same article show up multiple times in your inbox through Google alerts from different affiliate sources.
+            </p>
+            <p>
+              On a related note, if you see one of our scholars quoted in some little local paper, it's probably been syndicated. Look around the page and the byline and see if it says (Associated Press) or one of the other wire services.  If so, enter that service as the primary outlet.  You can still use the syndicated article link for the url.
+            </p>
+          </section>
+          <section>
+            <h3>N&V Category Column</h3>
+            <p>
+              This column is intended to help group entries together into no more than 3-5 distinct categories. Some categories are broad and will recur each week, such as "Scholar Publication" or "Briefings/Appearances." Others are determined by the media hits received that week. If Iran was a major story in the news and multiple scholars had media hits relating to it, you would create an "Iran" category.
+            </p>
+            <p>
+              You can create one or two specific region or issue categories when there are a lot of entries that would go under them. Usually that leaves a bunch of other uncategorized hits, which you can throw under a catch-all "Middle East" category.
+            </p>
+            <p>
+              Since you may not know what the most common regions or topics are for a given week until you're ready to fill out the newsletter, you may want to leave this column blank and decide on the categories Friday morning as you prepare to transfer data over to Constant Contact.
+            </p>
+            <p>
+              When you have all the categories filled in, you can sort the spreadsheet by this column to group all the entries together. This will make it easier to fill out the newsletter.
+            </p>
+          </section>
+          <section>
+            <h3>Scholar Column</h3>
+            <p>
+              Self-explanatory! You may want to sort the sheet by this column before sorting by category so that each of a scholar's entries appear close together.
+            </p>
+          </section>
+          <section>
+            <h3>Summary Column</h3>
+            <p>
+              The most useful way to fill this data in is to write out a description of the media hit as it will appear in the newsletter.  For example, if it's a quote in the New York Times, you would write, "was quoted in an article for the New York Times titled 'The Title of the Article'."
+            </p>
+            <p>
+              If you follow this format, you'll be able to simply copy and paste the text over into News & Views as you're filling out the Scholar Appearances section.  If you have multiple entries for the same person, you can chain the summary texts together to turn them into one brief paragraph covering each of the hits.
+            </p>
+          </section>
+          <section>
+            <h3>Link and Outlet Columns</h3>
+            <p>
+              A list of media appearances without any hyperlinks is dreadfully dull, so definitely include URLs in the spreadsheet whenever possible. Scholars do not alway include links in their self-reports, so you might have to go look up articles or broadcast appearances on Google News. It could also be that they conducted the interview, but that it will not appear online for days or weeks. In those cases, go ahead and include the entry in N&V without the link.
+            </p>
+            <p>
+              The Outlet column is pretty self-explanatory, although it could also mean the venue, in the case of public speaking appearances, or the audience in the case of testimony/briefings.
+            </p>
+          </section>
+          <section>
+            <h3>Good Quote Column</h3>
+            <p>
+              Most of the time you'll skip this column, but if you've got an entry that meets these conditions, make sure to drop in the quote text so we can highlight it in the newsletter and on our website:
+            </p>
+            <ol>
+              <li>It appears in a top-tier outlet like NYT, Washington Post, Foreign Policy, CNN, etc.</li>
+              <li>It's long enough to be a self-contained point. Someone reading it as an excerpt would have some idea what it's about.</li>
+              <li>Direct quotes are best. Skip paraphrases.</li>
+            </ol>
+          </section>
+          <section>
+            <h3>Title, Author, and Date Columns</h3>
+            <p>
+              Nothing unexpected in these. The reason we track authors (or for broadcast, interviewers) is that when we want to pitch scholars to provide analysis for journalists, we can look back to see if they have worked with those journalists previously.
+            </p>
+          </section>
+          <section>
+            <h3>Type Column</h3>
+            <p>
+              Select the one that best applies:
+            </p>
+            <ul>
+              <li>Written by Scholar</li>
+              <li>Mentioned</li>
+              <li>Quoted</li>
+              <li>Interview/TV/radio</li>
+              <li>Speaking Appearance</li>
+              <li>Briefing</li>
+              <li>Testimony</li>
             </ul>
           </section>
           <section>
-            <h3>'Scholars in the News' Section</h3>
+            <h3>MEI Mentioned? Column</h3>
             <p>
-              This is the original purpose of the News & Views newsletter--to draw attention to the weekly activities of our 40 Middle East scholars--and they do read this section closely to make sure their media hits were included, so be careful to add everything they send us!
-            </p>
-            <p>
-              The challenge is how to make this section not totally boring.  Using the Media Tracking sheet, add sections for the relevant categories (usually the name of the country that is the primary subject of the media hits) and list the individual Scholar records.  If one scholar has spoken on multiple topics, choose whichever you think is best and go ahead and mention the other hits within the same paragraph.
+              Scholars are asked to always mention their MEI affiliation to help promote MEI. Some outlets, especially on TV, will frequently neglect to mention MEI, but we like to keep track of this to make sure the scholars are remembering to try!
             </p>
           </section>
           <section>
-            <h3>Example Scholars Section Entries</h3>
-            <blockquote>
-              <p style="font-weight:bold;text-decoration:underline;color:black;">Syria</p>
-                <ul>
-                  <li><strong>Randa Slim</strong> was quoted in the <a href="#"></a><em>New York Times</em></a> article, "Syrian Fighting Gives Hezbollah New but Diffuse Purpose."</li>
-                  <li><strong>Paul Salem</strong> was interviewed by Nicholas Blanford of the Christian Science Monitor for the <a href="#">story</a>, "How the Ukraine crisis made Iran a better US partner on Syria." He was also interviewed by NPR's Deborah Amos for a forthcoming segment.</li>
-                  <li><strong>Daniel Serwer</strong> appeared on the Al Hurra program "Final Edition," commenting on Iraq and Syria. He also discussed Bosnia with Al Jazeera America.</li>
-                  <li><strong>Richard Murphy</strong> discussed events in Syria with Arise TV.</li>
-                </ul>
-            </blockquote>
+            <h3>Preparing for N&V</h3>
             <p>
-              Notice in the example above that entries with top-tier news outlets are put at the top, while the less well-known outlets fall to the bottom, especially if they don't have a hyperlink. Always post the more impressive and engaging ones higher.
+              When you're ready to start filling out News & Views, run a Sort on the scholars column as seen below, then do the same for the N&V Category column. This will group everything together nicely so that you can speed through the Scholars in the News section of the newsletter.
             </p>
+            <img src="assets/tutorials/img/SortSpreadsheet.png">
           </section>
           </div>
